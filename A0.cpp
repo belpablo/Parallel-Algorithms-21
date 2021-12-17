75e151e65af23b4a39133f8a5b36ded8785e58bd
Pavel <belpavl.2.0@gmail.com>
2021-12-17 20:31:17 +0300

    Add files via upload

--- A0.cpp
+++ A0.cpp
@@ -1,1 +1,29 @@
+﻿#include <iostream>
+using namespace std;
 
+int main()
+{
+    char s[80];
+    int slovo, count = 0;
+    system("chcp 1251");
+    system("cls");
+    cout << "enter your string here: " << endl;
+    cin.get(s, 80);
+    int i = 0;
+    while (s[i] == ' ' && s[i] != '\0')
+        i++;
+    slovo = 0;
+    while (s[i] != '\0') {
+        if (s[i] != ' ' && slovo == 0)
+        {
+            slovo = 1;
+            count++;
+        }
+        else if (s[i] == ' ')
+            slovo = 0;
+        i++;
+    }
+    cout << "Number of words per line: " << count;
+    cin.get(); cin.get();
+    return 0;
+}
