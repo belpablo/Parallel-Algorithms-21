e1ac584cd1484da3b371e98077e19c7d4b343e79
Pavel <belpavl.2.0@gmail.com>
2021-12-18 21:40:18 +0300

    Add files via upload

--- A1.cpp
+++ A1.cpp
@@ -6,43 +6,58 @@
 #include <typeinfo>
 #include <vector>
 #include <ctime>
+#include <omp.h>
 
 using namespace std;
 
 int main(int argc, char* argv[]) {
 
-	int num = atoi(argv[1]);
-    vector <int> vec(num);
+	int N = atoi(argv[1]);
+    vector <int> v(N);
 
     random_device rnd_device;
     mt19937 mersenne_engine{ rnd_device() };
-    uniform_int_distribution <int> dist{ 1, 52 };
-    auto gen = [&dist, &mersenne_engine]() {
-        return dist(mersenne_engine);
-    };
+    uniform_int_distribution <int> dist{ 1, 1000 };
+    auto gen = [&dist, &mersenne_engine]() { return dist(mersenne_engine); };
         
-    generate(begin(vec), end(vec), gen);
-        
-    int tmp = 0;
-    unsigned int start_time = clock();
-    for (auto i : vec) {
-        tmp += i;
-    }
-    unsigned int end_time = clock();
+    generate(begin(v), end(v), gen);
 
-    cout << "\ntime without openMP: " << end_time - start_time << endl;
+    for (int threads_num = 1; threads_num < 11; threads_num++) {
 
-    for (int i = 1; i < 11; i++) {
+        vector <double> thread_maxes(threads_num);
+        vector <int> thread_max_ids(threads_num);
 
-        tmp = 0;
-        start_time = clock();
-        #pragma omp parallel for num_threads(i)
-        for (auto j : vec) {
-            tmp += j;
+        double max_loc = 0;
+
+        clock_t start = clock();
+
+#pragma omp parallel for num_threads(threads_num) firstprivate(max_loc) 
+
+        for (int n = 0; n < N; n++) {
+
+            int thread_num = omp_get_thread_num();
+
+            if (v[n] > max_loc) {
+                max_loc = v[n];
+                thread_maxes[thread_num] = v[n];
+                thread_max_ids[thread_num] = n;
+            }
         }
-        end_time = clock();
 
-        cout << "\ntime with " << i << " threads: " << end_time - start_time << endl;
+        std::vector<double>::iterator max = std::max_element(thread_maxes.begin(), thread_maxes.end());
+
+        double max_val = *max;
+
+        int max_idx = thread_max_ids[max - thread_maxes.begin()];
+
+        clock_t end = clock();
+
+        double elapsed = double(end - start) / CLOCKS_PER_SEC;
+
+        printf("Number of threads = %d\n", threads_num);
+
+        printf("Execution time: %f ms\n\n", elapsed * 1000);
+
     }
 
     return 0;
