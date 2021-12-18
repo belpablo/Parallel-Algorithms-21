3482e11d93c264f36297f84944ad0ed3b4b21567
Pavel <belpavl.2.0@gmail.com>
2021-12-18 21:09:04 +0300

    Add files via upload

--- A0.cpp
+++ A0.cpp
@@ -7,22 +7,32 @@ using namespace std;
 // 
 // The input string is passed to the program through the terminal as the argv[1]
 // parameter.The program should display the number of words on the screen
+//
+// Compilation example: g++ -o A0.o A0.cpp
+// Launch example : A0.o "enter your string here"
 
 int main(int argc, char* argv[])
 {
-    string const separators{ " \t\n" };
-    string input_string = argv[1];
-    cout << "\ninput string: " << input_string << endl;
-
-    size_t begin, pos = 0;
-    int res = 0;
-
-    while ((begin = input_string.find_first_not_of(separators, pos)) != std::string::npos)
+    if (argc != 2)
     {
-        pos = input_string.find_first_of(separators, begin + 1);
-        res++;
-    }
+        cout << "\ninvalid input data" << endl;
+    } 
+    else 
+    {
+        string const separators{ " \t\n" };
+        string input_string = argv[1];
+        cout << "\ninput string: " << input_string << endl;
 
-    cout << "the number of words on the screen: " << res << endl;
+        size_t begin, pos = 0;
+        int res = 0;
+
+        while ((begin = input_string.find_first_not_of(separators, pos)) != std::string::npos)
+        {
+            pos = input_string.find_first_of(separators, begin + 1);
+            res++;
+        }
+
+        cout << "the number of words on the screen: " << res << endl;
+    }
     return 0;
 }
