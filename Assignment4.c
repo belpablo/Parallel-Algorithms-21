ffedd85861ab9965b02ae07b5caf8753d4acb500
Pavel <belpavl.2.0@gmail.com>
2021-12-22 13:01:24 +0300

    Update Assignment4.c

--- Assignment4.c
+++ Assignment4.c
@@ -1,21 +1,28 @@
+#include <iostream>
 #include <stdio.h>
 #include "mpi.h"
+using namespace std;
+
 int main(int argc, char* argv[]) {
 	MPI_Init(&argc, &argv);
-	int rank, n, i, message;
+	int rank, n, i;
 	MPI_Status status;
 	MPI_Comm_size(MPI_COMM_WORLD, &n);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	if (rank == 0)
+	double rank_d = double(rank), message;
+	int root_process = 4;
+
+	if (rank == root_process)
 	{
 		cout << "Hello from process " << rank << "\n";
 		for (i = 1; i < n; i++) {
-			MPI_Recv(&message, 1, MPI_INT, MPI_ANY_SOURCE,
-			         MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-			cout << "Hello from process " << message << endl;
+			MPI_Recv(&message, 1, MPI_DOUBLE, MPI_ANY_SOURCE,
+				MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+			cout << "Hello from process " << message << " received by process " << rank << endl;
 		}
 	}
-	else MPI_Send(&rank, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+	else MPI_Send(&rank_d, 1, MPI_DOUBLE, root_process, 0, MPI_COMM_WORLD);
+
 	MPI_Finalize();
 	return 0;
 }
